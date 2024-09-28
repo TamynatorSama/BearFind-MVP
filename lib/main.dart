9835f574459cbdd75dae5269843159e53e5141b3
TamynatorSama <kolawoletamilore1@gmail.com>
2024-09-28 09:19:42 +0100

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lost_items/controller/repository/auth_repo.dart';
+import 'package:lost_items/firebase_options.dart';
 import 'package:lost_items/pages/auth/email_page.dart';
 import 'package:lost_items/pages/select_action.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await AuthRepository.instance.init();
+ await AuthRepository.instance.init(); 
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  
   runApp(const MyApp());
 }
 
